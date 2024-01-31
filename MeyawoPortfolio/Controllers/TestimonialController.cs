711f51fcb2e20f3ca54cd643070e8e32d19b403f
Yağmur K. <yagmurrttk@gmail.com>
2024-01-31 16:50:35 +0300

    Testimonial Crud

--- MeyawoPortfolio/Controllers/TestimonialController.cs
+++ MeyawoPortfolio/Controllers/TestimonialController.cs
@@ -15,5 +15,41 @@ namespace MeyawoPortfolio.Controllers
             var values = db.Tbl_Testimonial.ToList();
             return View(values);
         }
+        [HttpGet]
+        public ActionResult AddTestimonial()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddTestimonial(Tbl_Testimonial testimonial)
+        {
+            db.Tbl_Testimonial.Add(testimonial);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult DeleteTestimonial(int id)
+        {
+            var testimonial = db.Tbl_Testimonial.Find(id);
+            db.Tbl_Testimonial.Remove(testimonial);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult UpdateTestimonial(int id)
+        {
+            var testimonial = db.Tbl_Testimonial.Find(id);
+            return View(testimonial);
+        }
+        [HttpPost]
+        public ActionResult UpdateTestimonial(Tbl_Testimonial p)
+        {
+            var testimonial = db.Tbl_Testimonial.Find(p.TestimonialID);
+            testimonial.NameSurname = p.NameSurname;
+            testimonial.Description = p.Description;
+            testimonial.Image = p.Image;
+            testimonial.Status = p.Status;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
